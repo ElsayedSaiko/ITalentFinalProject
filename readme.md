b8d6b3ec9864fd5e59e934d3d3405d19c70ba3c6
Elsayed <elsayedhosny88@gmail.com>
2019-06-20 11:17:58 +0200

    first commit

---
+++ readme.md
@@ -0,0 +1,48 @@
+<p align="center">Italaent</p>
+
+<p align="center">
+It's social network to discover various young artistic talents.
+</p>
+
+## About italent project
+
+Italent project aims to design a new social network system to discover various young artistic talents. It is a social network through which the talents publish videos/images/text of his talent and audience can access it and rate it. Also, the systems enables making competitions among the talents and the winners are chosen by committee of expert users.
+
+- Reach the audience every where.
+- Focus on talented.
+- Motivate young talents.
+- Wide discovering of young talents .
+- Adopt talents
+
+
+## contents
+
+- Admin pannel and seperate roles between 4 users.
+- newsfeed page.
+- profile page.
+- add friends & remove friends.
+- like unlike posts.
+- comments on posts.
+
+
+## Requirements 
+
+- PHP >= 7.0.0
+- OpenSSL PHP Extension
+- PDO PHP Extension
+- Mbstring PHP Extension
+- Tokenizer PHP Extension
+- XML PHP Extension
+
+## Installation
+
+Just clone the project to anywhere in your computer. 
+Run ` composer install ` <br>
+Then ` npm install ` <br>
+and ` php artisan migrate `
+
+Now you are done. 
+<br>
+
+` php artisan serve ` and open the project on the browser. 
+
---
+++ routes/api.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('posts','PostsApiController@index');
+Route::get('posts/{id}','PostsApiController@show');
+Route::post('posts','PostsApiController@store');
+Route::post('posts/{id}','PostsApiController@update');
+Route::delete('posts/delete/{id}','PostsApiController@delete');
+Route::get('categories','CategoriesApiController@index');
+Route::get('categories/{id}', 'CategoriesApiController@showAll');
+Route::get('users','UserApiController@index');
+Route::get('user/{id}','UserApiController@showUser');
+Route::post('user/{id}','UserApiController@update');
+Route::get('liked','LikesApiController@index');
+Route::get('comments','PostsCommentsApiController@index');
+Route::get('comments/{id}','PostsCommentsApiController@show');
+Route::post('comments/{id}','PostsCommentsApiController@update');
+Route::post('comments','PostsCommentsApiController@store');
+Route::delete('comments/delete/{id}','PostsCommentsApiController@delete');
+
+
+
